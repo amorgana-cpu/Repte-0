9b8072cb57137a42080dd7eea52a7b478a02836c
= <=>
2025-10-23 12:26:12 +0200

    MD3.2.Ampliaciódeconeixements

---
+++ MD3.2.Ampliaciódeconeixements/indexOf.js
@@ -0,0 +1,1 @@
+console.log("En quina posició d'aquest text es troba la paraula final".indexOf("final"));
---
+++ MD3.2.Ampliaciódeconeixements/replace.js
@@ -0,0 +1,5 @@
+let a = "1111100011";
+a = a.replace(/0/g, "1");
+a = a.replace(/1/g, "2");
+a = a.replace(/2/g, "3");
+console.log(a);
