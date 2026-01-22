bd8d076e5eb829336d80afd0dcbe104ad3de970a
= <=>
2026-01-22 11:50:55 +0100

    Primer commit

---
+++ MD8/ordenarelsvalorsporvalorsex.js
@@ -0,0 +1,11 @@
+const dicc1 = {hola: 1, adios: 2, 3: 100, 4: 12};
+
+
+// Convertim a array de parells i ordenem segons el valor
+const diccOrdenatArray = Object.entries(dicc1).sort((a, b) => a[1] - b[1]);
+
+
+console.log(diccOrdenatArray);
+
+
+
